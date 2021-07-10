d1b22dc53e5425d8d6c8342eb295b5536f5983cb
ravalika-919 <69677551+ravalika-919@users.noreply.github.com>
2021-07-10 19:07:41 +0530

    Add files via upload

---
+++ JAVA/Array programs/main.java
@@ -0,0 +1,11 @@
+class Main
+{
+    // Program to convert a primitive character array to a string
+    public static void main(String[] args)
+    {
+        char[] charArray = { 'J', 'a', 'v', 'a' };
+ 
+        String str = new String(charArray);
+        System.out.println(str);
+    }
+}
