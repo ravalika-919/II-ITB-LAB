d425c0d7aefaecc5b0caed8e42dedf377c6a7d9c
ravalika-919 <69677551+ravalika-919@users.noreply.github.com>
2021-07-10 19:19:33 +0530

    Add files via upload

---
+++ JAVA/Basic Programs/evenodd.java
@@ -0,0 +1,15 @@
+public class EvenOdd
+{
+public static void main(String[] args)
+{
+int a=9;
+if(a%2==0)
+{
+System.out.println("Even Number");
+}
+else
+{
+System.out.println("Odd Number");
+}
+}
+}
