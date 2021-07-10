0b3be8626bab645f663c03b2efa47bebee31419a
ravalika-919 <69677551+ravalika-919@users.noreply.github.com>
2021-07-10 19:17:46 +0530

    Add files via upload

---
+++ JAVA/Basic Programs/compoundinterest.java
@@ -0,0 +1,9 @@
+class CompoundInterest
+{
+public static void main(String[] args)
+{
+double principle=100,rate=100,time=20,CompoundInterest;
+CompoundInterest=principle*(Math.pow((1+rate/100),time));
+System.out.println(CompoundInterest);
+}
+}
